345f08bf066db387171dd56431533e88e36ee331
536uriel <uribrave1@gmail.com>
2025-03-31 12:46:04 +0300

    first commit

---
+++ codeMirror.js
@@ -0,0 +1,130 @@
+//for p5 canvas
+
+var body = document.body,
+    html = document.documentElement;
+
+var cwidth = Math.max(body.scrollWidth, body.offsetWidth,
+    html.clientWidth, html.scrollWidth, html.offsetWidth);
+
+var cheight = Math.max(body.scrollHeight, body.offsetHeight,
+    html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+var callback = () => { };
+
+function setup() {
+
+    let canvas = createCanvas(500, 400);
+    canvas.parent("output-canvas");
+
+}
+
+function draw() {
+    background(225);
+    callback();
+}
+
+
+// Initialize CodeMirror
+
+//editor for the precode 
+var editor1 = CodeMirror.fromTextArea(document.getElementById("editor1"), {
+    mode: "javascript",  // Syntax highlighting mode
+    lineNumbers: true,   // Show line numbers
+    theme: "default"
+});
+
+//editor for the draw function
+var editor2 = CodeMirror.fromTextArea(document.getElementById("editor2"), {
+    mode: "javascript",  // Syntax highlighting mode
+    lineNumbers: true,   // Show line numbers
+    theme: "default"
+
+});
+
+
+
+editor1.setSize(cwidth / 2, 120);
+editor2.setSize(cwidth / 2, 200);
+
+
+
+
+
+var commands = [
+    "background(225)",
+    "fill(0, 0, 225)",
+    "circle(x, y, r)",
+    "rect(x, y, w, h)",
+    "line(x1, y1, x2, y2)",
+    "random()",
+    "keyIsPressed",
+    "key",
+    `if(){  
+    
+
+}else{ 
+
+
+}`
+]
+
+var clist = document.getElementById("commands");
+
+commands.forEach(command => {
+    let li = document.createElement("li");
+
+    li.innerText = command;
+
+    li.addEventListener("click", () => {
+        editor2.setValue(editor2.getValue() + "\n" + command);
+    })
+
+    clist.appendChild(li)
+});
+
+
+
+
+// Function to execute code as online console
+function runCode() {
+    try {
+        // Get the code from CodeMirror
+        var preCode = editor1.getValue();
+        var code = editor2.getValue();
+
+        // Redirect console.log to display in the output div
+        var outputDiv = document.getElementById("output");
+        outputDiv.innerHTML = ''; // Clear previous output
+
+        var originalConsoleLog = console.log;
+        console.log = function (message) {
+            outputDiv.innerHTML += message + '<br>';
+            originalConsoleLog.apply(console, arguments);
+        };
+
+
+        //for the output div 
+        new Function(preCode)();
+        new Function(code)();
+
+        // Run the JavaScript code dinamicly 
+        /*
+        new Function(code)();
+        is equelevant to: 
+        function(){
+            console.log("code")
+        }
+        */
+        callback = () => {
+            //for execute p5 code
+            new Function(code)();
+        }
+
+
+
+        // Restore original console.log
+        console.log = originalConsoleLog;
+    } catch (error) {
+        document.getElementById("output").innerHTML = "Error: " + error.message;
+    }
+}
